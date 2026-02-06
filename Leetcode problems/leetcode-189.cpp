9bb968c3b1e569af1ee693dc5944cf4befec34e6
SHIVAKUMAR <shivakumarbirapur@gmail.com>
2026-02-06 22:54:54 +0530

    Add files via upload

---
+++ Leetcode problems/leetcode-189.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  void rotate(vector<int> &nums, int k)
+  {
+    int n = nums.size();
+
+    // Handle cases where k >= n
+    k = k % n;
+
+    // Step 1: Reverse the entire array
+    reverse(nums.begin(), nums.end());
+
+    // Step 2: Reverse the first k elements
+    reverse(nums.begin(), nums.begin() + k);
+
+    // Step 3: Reverse the remaining elements
+    reverse(nums.begin() + k, nums.end());
+  }
+};
